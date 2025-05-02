d1ab6c9efa8a29393c27384f1524332021f94b1c
Sumit Zalake <sumit.zalake@neutrinotechlabs.com>
2025-05-02 12:47:47 +0530

    modify main.py

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 # Dependency to get DB session
+# Dependency to get DB session
+# Dependency to get DB session
 def get_db():
     db = SessionLocal()
     try:
